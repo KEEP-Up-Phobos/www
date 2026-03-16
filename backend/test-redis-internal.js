3c882a9da6498ace0ec40d463f67fe45c958dba9
KEEP-Up Dev <dev@keepup.local>
2026-03-16 00:27:47 -0300

    chore: remove dead code, test scripts, legacy fetchers and serpents-service

--- backend/test-redis-internal.js
+++
@@ -1,6 +0,0 @@
-
-const Redis = require('ioredis');
-const client = new Redis('redis://redis:6379');
-client.on('error', console.error);
-client.on('connect', () => { console.log('Connected!'); client.disconnect(); });
-client.ping().then(console.log).catch(console.error);
